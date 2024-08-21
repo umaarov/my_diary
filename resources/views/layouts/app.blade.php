039c3b58d2dc19b3e0a94676db72322ea8e95c97
Ismoiljon <hs.umarov21@gmail.com>
2024-08-21 21:14:52 +0500

    push 8

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/default.min.css">
+    <script src="https://cdn.ckeditor.com/4.19.1/standard/ckeditor.js"></script> --}}
     <title>@yield('title', "Umarov's Diary")</title>
 </head>
 <body>
@@ -17,5 +19,8 @@
     <main>
         @yield('content')
     </main>
+
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"></script>
+    <script>hljs.highlightAll();</script> --}}
 </body>
 </html>
