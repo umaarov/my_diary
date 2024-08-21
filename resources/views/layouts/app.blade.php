ee09a56696504e2ad9206875b55d089e066082ee
Ismoiljon <hs.umarov21@gmail.com>
2024-08-21 20:03:25 +0500

    push 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Umarov\'s Diary')</title>
+    <title>@yield('title', "Umarov's Diary")</title>
 </head>
 <body>
     <header>
@@ -16,7 +16,6 @@
     <main>
         <nav>
             <ul>
-                <li><a href="{{ route('posts.index') }}">Home</a></li>
                 @yield('breadcrumb')
             </ul>
         </nav>
